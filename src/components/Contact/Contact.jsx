bb18798c6353f79b9c790cd2d3a0a9888f163026
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-06 20:57:33 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 import { Btn } from './Contact.styled';
 import { Number } from './Contact.styled';
 
-const Contact = ({ name, number, id, onDeleteContact }) => (
-  <>
-    <p>
-      {name}: <Number>{number}</Number>
-    </p>
-    <Btn type="button" onClick={() => onDeleteContact(id)}>
-      Delete
-    </Btn>
-  </>
-);
+const Contact = ({ name, number, id }) => {
+  const dispatch = useDispatch();
+
+  const handelDelete = () => dispatch(deleteContact(id));
+
+  return (
+    <>
+      <p>
+        {name}: <Number>{number}</Number>
+      </p>
+      <Btn type="button" onClick={handelDelete}>
+        Delete
+      </Btn>
+    </>
+  );
+};
 
 export default Contact;
 
@@ -19,5 +27,4 @@ Contact.propTypes = {
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
