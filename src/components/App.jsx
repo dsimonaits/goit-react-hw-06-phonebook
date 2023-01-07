9eb9e0472e62a0e3a339f3004ef6c87c6a412b81
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-07 21:50:03 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,17 @@
-import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selectors';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import FilterByName from './Filter';
 import ContactList from './ContactList';
 
 const App = () => {
-  const contacts = useSelector(getContacts);
-
   return (
     <>
       <Section title="Phonebook" border="false">
         <ContactForm />
       </Section>
-      <Section title="Contacts" border="true">
-        {contacts.length === 0 ? (
-          <p>Sorry your contact list is empty. Add someone.</p>
-        ) : (
-          <FilterByName />
-        )}
 
+      <Section title="Contacts" border="true">
+        <FilterByName />
         <ContactList />
       </Section>
     </>
